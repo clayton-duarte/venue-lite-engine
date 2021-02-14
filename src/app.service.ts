833c6c405b6f721345bbd46aafa8be658ac880c3
clayton-duarte <clayton@null.net>
2021-02-13 23:17:01 -0500

    :sparkles: Ping endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  ping(): string {
+    // TODO > ping response
+    return 'ok';
   }
 }
