1278d971be7e75c9a8e8be1bcbd659c4f0a579bd
clayton-duarte <clayton@null.net>
2021-02-14 23:02:53 -0500

    :gear: Configs swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
 
@@ -6,6 +7,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .setTitle('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
 
